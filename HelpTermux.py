02625bc01859878171df548af95a25d7e8626bd3
LCA-HACK <86206133+LCA-HACK@users.noreply.github.com>
2021-07-07 16:10:46 -0500

    Update HelpTermux.py

--- HelpTermux.py
+++ HelpTermux.py
@@ -14,11 +14,11 @@ os.system("clear")
 
 def baner():
 
-        print(RED+"| |     _____   ____")
-        print(RED+"| |    | ___|  | __ |")
-        print(RED+"| |    | |     ||__||")
-        print(RED+"| |__  | |__   ||  ||")
-        print(RED+"|____| |___|   ||  ||")
+        print(RED+"| |      _____    ____")
+        print(RED+"| |     | ___|   | __ |")
+        print(RED+"| |     | |      ||__||")
+        print(RED+"| |__   | |__    ||  ||")
+        print(RED+"|____|  |____|   ||  ||")
 baner()
 
 print("\nCreado por L.C.A HACK")
@@ -27,14 +27,19 @@ while True:
         print(YELLOW+"[1] Instalar paquetes basicos")
         print("\n[2] Actualizar Termux")
         print("\n[3] Dar permisos de  almacenamiento a termux")
-        print("\n[4] Salir")
+        print("\n[4] Actualizar Paquetes")
+        print("\n[5] Salir")
         opcion=input("\nElije una opcion: ")
         if opcion=="1":
-                os.system("pkg install -y git; pkg install -y python; pkg install -y python2; pkg install -y php; pkg install -y openssh; pkg  install -y wget; pkg install -y vim;")
+                os.system("pkg install -y git; pkg install -y python; pkg install -y python2; pkg install -y php; pkg install -y openssh; pkg  install -y wget; pkg install -y vim; pkg install -y curl")
         if opcion=="2":
                 os.system("clear")
                 os.system("termux-change-repo")
         if opcion=="3":
                 os.system("clear")
                 os.system("termux-setup-storage")
+        if opcion=="4":
+                os.system("pkg update; pkg upgrade")
+        if opcion=="5":
+                print ("Hasta luego")
         break
